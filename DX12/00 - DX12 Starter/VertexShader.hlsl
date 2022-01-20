d7350c05c05bb696f1b714d2d58207b5a9a80181
Chris Cascioli <chriscascioli@gmail.com>
2022-01-20 12:42:35 -0500

    Updated DX12 vertex shader to fix issue on AMD mobile cards.  Removed offset which is no longer being used in this demo.

--- DX12/00 - DX12 Starter/VertexShader.hlsl
+++ DX12/00 - DX12 Starter/VertexShader.hlsl
@@ -1,9 +1,4 @@
 
-cbuffer ExternalData : register(b0)
-{
-	float3 offset;
-};
-
 // Struct representing a single vertex worth of data
 // - This should match the vertex definition in our C++ code
 // - By "match", I mean the size, order and number of members
@@ -56,7 +51,7 @@ VertexToPixel main( VertexShaderInput input )
 	// - Each of these components is then automatically divided by the W component, 
 	//   which we're leaving at 1.0 for now (this is more useful when dealing with 
 	//   a perspective projection matrix, which we'll get to in the future).
-	output.screenPosition = float4(input.localPosition + offset, 1.0f);
+	output.screenPosition = float4(input.localPosition, 1.0f);
 
 	// Pass the color through 
 	// - The values will be interpolated per-pixel by the rasterizer
